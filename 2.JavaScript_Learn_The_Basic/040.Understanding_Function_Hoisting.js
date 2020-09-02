1ea8b05dac0d020aead97b8a55516eb2518277d4
powerseunghun <size411@naver.com>
2020-09-03 03:30:34 +0900

    Part2-Basic

---
+++ 2.JavaScript_Learn_The_Basic/040.Understanding_Function_Hoisting.js
@@ -0,0 +1,13 @@
+hello();
+function hello() {
+    console.log("Hello");
+}
+
+// hello2 is not a function
+hello2();
+// var hello2 = function() {
+//     console.log("Hello2");
+// }
+function hello2() {
+    console.log("Hello2");
+}
